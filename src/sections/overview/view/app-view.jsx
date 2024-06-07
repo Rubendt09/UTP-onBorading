c60344b28c7c7cd452a7c9fa01050da5a54db08c
Ruben Dominguez Trujillo <rubendomingueztrujillo@192.168.1.11>
2024-06-07 16:45:32 -0500

    vista de inicio

--- src/sections/overview/view/app-view.jsx
+++ src/sections/overview/view/app-view.jsx
@@ -1,230 +1,134 @@
-import { faker } from '@faker-js/faker';
-
+import React from 'react';
 import Container from '@mui/material/Container';
-import Grid from '@mui/material/Unstable_Grid2';
 import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
+import Button from '@mui/material/Button';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import Grid from '@mui/material/Grid';
 
-import Iconify from 'src/components/iconify';
-
-import AppTasks from '../app-tasks';
-import AppNewsUpdate from '../app-news-update';
-import AppOrderTimeline from '../app-order-timeline';
-import AppCurrentVisits from '../app-current-visits';
-import AppWebsiteVisits from '../app-website-visits';
-import AppWidgetSummary from '../app-widget-summary';
-import AppTrafficBySite from '../app-traffic-by-site';
-import AppCurrentSubject from '../app-current-subject';
-import AppConversionRates from '../app-conversion-rates';
+import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
+import { Avatar } from '@mui/material';
+import Router from 'src/routes/sections';
 
 // ----------------------------------------------------------------------
 
 export default function AppView() {
   return (
-    <Container maxWidth="xl">
-      <Typography variant="h4" sx={{ mb: 5 }}>
-        Hi, Welcome back 👋
+    <Container maxWidth="100%">
+      <Typography variant="h4" sx={{ mb: 3 }}>
+        Hola, Ruben Dominguez 👋
       </Typography>
 
-      <Grid container spacing={3}>
-        <Grid xs={12} sm={6} md={3}>
-          <AppWidgetSummary
-            title="Weekly Sales"
-            total={714000}
-            color="success"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_bag.png" />}
-          />
-        </Grid>
+      <Card sx={{ p: 2, boxShadow: 3, borderRadius: 2 }}>
+        <Grid container alignItems="center" spacing={2}>
+          <Grid item xs={6}>
+            <Typography variant="h4" sx={{ mb: 1 }}>
+              Charla de cultura UTP
+            </Typography>
+            <Typography variant="h6" color="text.secondary" sx={{ mb: 2 }}>
+              18:30 - 20:00
+            </Typography>
+          </Grid>
 
-        <Grid xs={12} sm={6} md={3}>
-          <AppWidgetSummary
-            title="New Users"
-            total={1352831}
-            color="info"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_users.png" />}
-          />
+          <Grid item xs={6} container direction="column" alignItems="flex-end">
+            <Chip label="Presencial" color="primary" variant="outlined" sx={{ mb: 1 }} />
+            <Typography variant="body2" color="text.secondary">
+              Auditorio Principal
+            </Typography>
+          </Grid>
         </Grid>
+        <Box sx={{ mt: 2, width: '100%' }}>
+          <Button
+            variant="contained"
+            color="primary"
+            startIcon={<CalendarTodayIcon />}
+            sx={{ textTransform: 'none' }}
+            fullWidth
+          >
+            Ver mi horario
+          </Button>
+        </Box>
+      </Card>
 
-        <Grid xs={12} sm={6} md={3}>
-          <AppWidgetSummary
-            title="Item Orders"
-            total={1723315}
-            color="warning"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_buy.png" />}
-          />
+      <Card sx={{ p: 2, boxShadow: 3, borderRadius: 2, mt: 4, backgroundColor: '#f0f4fa' }}>
+        <Grid container alignItems="center" spacing={1}>
+          <Grid item xs={2} container direction="column" alignItems="center">
+            <Avatar sx={{ backgroundColor: '#e0e7ff', color: '#5c6bc0' }}>
+              <PushPinOutlinedIcon />
+            </Avatar>
+          </Grid>
+          <Grid item xs={10}>
+            <Typography variant="h6" color="text.secondary" sx={{ mb: 1 }}>
+              Estas inscrito en:
+            </Typography>
+            <Typography variant="h5">Modalidad semipresencial - 50% virtual</Typography>
+          </Grid>
         </Grid>
+      </Card>
 
-        <Grid xs={12} sm={6} md={3}>
-          <AppWidgetSummary
-            title="Bug Reports"
-            total={234}
-            color="error"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />}
-          />
-        </Grid>
+      {/** Contenido de app */}
 
-        <Grid xs={12} md={6} lg={8}>
-          <AppWebsiteVisits
-            title="Website Visits"
-            subheader="(+43%) than last year"
-            chart={{
-              labels: [
-                '01/01/2003',
-                '02/01/2003',
-                '03/01/2003',
-                '04/01/2003',
-                '05/01/2003',
-                '06/01/2003',
-                '07/01/2003',
-                '08/01/2003',
-                '09/01/2003',
-                '10/01/2003',
-                '11/01/2003',
-              ],
-              series: [
-                {
-                  name: 'Team A',
-                  type: 'column',
-                  fill: 'solid',
-                  data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
-                },
-                {
-                  name: 'Team B',
-                  type: 'area',
-                  fill: 'gradient',
-                  data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
-                },
-                {
-                  name: 'Team C',
-                  type: 'line',
-                  fill: 'solid',
-                  data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
-                },
-              ],
-            }}
-          />
-        </Grid>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: { xs: 'column', md: 'row' }, 
+          alignItems: 'center',
+          justifyContent: 'center',
+          mt: 4,
+          flexWrap: 'wrap',
+          gap: 4, 
+        }}
+      >
+        <Card
+          sx={{
+            width: '400px',
+            height: '220px',
+            boxShadow: 3,
+            borderRadius: 2,
+            mb: 4, 
+            backgroundImage: 'url(/assets/references/utpclass.png)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+          onClick={() => {
+            window.location.href = 'https://class.utp.edu.pe/';
+          }}
+        />
 
-        <Grid xs={12} md={6} lg={4}>
-          <AppCurrentVisits
-            title="Current Visits"
-            chart={{
-              series: [
-                { label: 'America', value: 4344 },
-                { label: 'Asia', value: 5435 },
-                { label: 'Europe', value: 1443 },
-                { label: 'Africa', value: 4443 },
-              ],
-            }}
-          />
-        </Grid>
+        <Card
+          sx={{
+            width: '400px',
+            height: '220px',
+            boxShadow: 3,
+            borderRadius: 2,
+            mb: 4, // Espacio entre las tarjetas
+            backgroundImage: 'url(/assets/references/utpportal.png)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+          onClick={() => {
+            window.location.href = 'https://portal.utp.edu.pe/';
+          }}
+        />
 
-        <Grid xs={12} md={6} lg={8}>
-          <AppConversionRates
-            title="Conversion Rates"
-            subheader="(+43%) than last year"
-            chart={{
-              series: [
-                { label: 'Italy', value: 400 },
-                { label: 'Japan', value: 430 },
-                { label: 'China', value: 448 },
-                { label: 'Canada', value: 470 },
-                { label: 'France', value: 540 },
-                { label: 'Germany', value: 580 },
-                { label: 'South Korea', value: 690 },
-                { label: 'Netherlands', value: 1100 },
-                { label: 'United States', value: 1200 },
-                { label: 'United Kingdom', value: 1380 },
-              ],
-            }}
-          />
-        </Grid>
-
-        <Grid xs={12} md={6} lg={4}>
-          <AppCurrentSubject
-            title="Current Subject"
-            chart={{
-              categories: ['English', 'History', 'Physics', 'Geography', 'Chinese', 'Math'],
-              series: [
-                { name: 'Series 1', data: [80, 50, 30, 40, 100, 20] },
-                { name: 'Series 2', data: [20, 30, 40, 80, 20, 80] },
-                { name: 'Series 3', data: [44, 76, 78, 13, 43, 10] },
-              ],
-            }}
-          />
-        </Grid>
-
-        <Grid xs={12} md={6} lg={8}>
-          <AppNewsUpdate
-            title="News Update"
-            list={[...Array(5)].map((_, index) => ({
-              id: faker.string.uuid(),
-              title: faker.person.jobTitle(),
-              description: faker.commerce.productDescription(),
-              image: `/assets/images/covers/cover_${index + 1}.jpg`,
-              postedAt: faker.date.recent(),
-            }))}
-          />
-        </Grid>
-
-        <Grid xs={12} md={6} lg={4}>
-          <AppOrderTimeline
-            title="Order Timeline"
-            list={[...Array(5)].map((_, index) => ({
-              id: faker.string.uuid(),
-              title: [
-                '1983, orders, $4220',
-                '12 Invoices have been paid',
-                'Order #37745 from September',
-                'New order placed #XF-2356',
-                'New order placed #XF-2346',
-              ][index],
-              type: `order${index + 1}`,
-              time: faker.date.past(),
-            }))}
-          />
-        </Grid>
-
-        <Grid xs={12} md={6} lg={4}>
-          <AppTrafficBySite
-            title="Traffic by Site"
-            list={[
-              {
-                name: 'FaceBook',
-                value: 323234,
-                icon: <Iconify icon="eva:facebook-fill" color="#1877F2" width={32} />,
-              },
-              {
-                name: 'Google',
-                value: 341212,
-                icon: <Iconify icon="eva:google-fill" color="#DF3E30" width={32} />,
-              },
-              {
-                name: 'Linkedin',
-                value: 411213,
-                icon: <Iconify icon="eva:linkedin-fill" color="#006097" width={32} />,
-              },
-              {
-                name: 'Twitter',
-                value: 443232,
-                icon: <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={32} />,
-              },
-            ]}
-          />
-        </Grid>
-
-        <Grid xs={12} md={6} lg={8}>
-          <AppTasks
-            title="Tasks"
-            list={[
-              { id: '1', name: 'Create FireStone Logo' },
-              { id: '2', name: 'Add SCSS and JS files if required' },
-              { id: '3', name: 'Stakeholder Meeting' },
-              { id: '4', name: 'Scoping & Estimations' },
-              { id: '5', name: 'Sprint Showcase' },
-            ]}
-          />
-        </Grid>
-      </Grid>
+        <Card
+          sx={{
+            width: '400px',
+            height: '220px',
+            boxShadow: 3,
+            borderRadius: 2,
+            mb: 4, // Espacio entre las tarjetas
+            backgroundImage: 'url(/assets/references/utpinfo.png)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+          onClick={() => {
+            window.location.href = 'https://info.utp.edu.pe/';
+          }}
+        />
+      </Box>
     </Container>
   );
 }
