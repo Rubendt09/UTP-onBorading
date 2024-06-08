2fc2f895c2951cca397639893ef60442c1995b69
Ruben Dominguez Trujillo <rubendomingueztrujillo@192.168.1.11>
2024-06-07 21:00:41 -0500

    nav bar

--- src/layouts/dashboard/config-navigation.jsx
+++ src/layouts/dashboard/config-navigation.jsx
@@ -25,28 +25,28 @@ const navConfig = [
   {
     title: 'Cursos',
     path: '/courses',
-    icon: icon('ic_cart'),
+    icon: icon('courses'),
   },
   {
     title: 'product',
     path: '/products',
     icon: icon('ic_cart'),
   },
-  {
+  {/*
     title: 'blog',
     path: '/blog',
     icon: icon('ic_blog'),
-  },
+  */},
   {
-    title: 'login',
+    title: 'Cerrar Sesion',
     path: '/login',
     icon: icon('ic_lock'),
   },
-  {
+  {/*
     title: 'Not found',
     path: '/404',
     icon: icon('ic_disabled'),
-  },
+*/},
 ];
 
 export default navConfig;
